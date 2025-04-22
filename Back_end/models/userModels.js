5d7c50ab16a4017a022ef1d6a2ee6c65a7042367
laila ait bella <aitbellalaila3@hotmail.com>
2025-04-22 21:14:00 +0200

    Modif temporaire sur userModels.js

--- Back_end/models/userModels.js
+++ Back_end/models/userModels.js
@@ -1,19 +1,15 @@
 import mongoose from 'mongoose';
-const usersShema=new mongoose.Schema(
-    {
-        cin:String,
-        password: String,
-        password_hash:String,
-<<<<<<< HEAD
-        name:String,
-=======
->>>>>>> 2483e50dc7cb9aefde17381336324d4ab4ac7d61
-        mail:String,
-        telephone:Number,
-        role: {type : String, enum:["admin","user"],default : "user"},
-        rib:{type:String}
-    }
-);
-const User=mongoose.model("User",usersShema)
-export default User
 
+const usersShema = new mongoose.Schema({
+    cin: String,
+    password: String,
+    password_hash: String,
+    name: String,
+    mail: String,
+    telephone: Number,
+    role: { type: String, enum: ["admin", "user"], default: "user" },
+    rib: { type: String }
+});
+
+const User = mongoose.model("User", usersShema);
+export default User;
